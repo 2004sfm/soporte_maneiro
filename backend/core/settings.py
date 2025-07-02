31e1008d6631e5ba3cefaf33442ef1766adc6153
2004sfm <2004sfm@gmail.com>
2025-07-02 12:03:55 -0400

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db('DATABASE_URL')
+    'default': env.db('POSTGRES_URL')
 }
 
 
